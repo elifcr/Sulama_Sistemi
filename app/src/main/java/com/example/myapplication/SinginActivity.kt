022eb031d8171536ec7742ef5e48b0ccf7f468cf
Elif Car <95692232+elifcr@users.noreply.github.com>
2024-04-05 02:31:17 +0300

    Kayit Ol

--- app/src/main/java/com/example/myapplication/SinginActivity.kt
+++ app/src/main/java/com/example/myapplication/SinginActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,4 +18,6 @@ class SinginActivity : AppCompatActivity() {
             insets
         }
     }
+
+    fun goToLogin(view: View) {}
 }
